737ab0e8405f090058cea392304ff8892bacdffb
Amol Wankhede <hello@amolwankhede.com>
2016-06-22 01:23:03 +1200

    Seasons Agents
    add activiy city heading

---
+++ connect.inc.php
@@ -0,0 +1,41 @@
+<?php 
+    $host = "localhost";
+    $database = "seasons";
+    $username = "root";
+    $password = "root";
+
+    try {
+        $pdo = new PDO("mysql:host=" .$host. ";dbname=$database", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->exec("SET NAMES 'utf8'");
+        return $pdo;
+    }
+    catch (PDOException $e)     {
+        echo "<div class='alert alert-danger'>ERROR : " . $e->getMessage() . "</div>";
+        exit();
+    }
+
+
+public function vardump($v) {
+    echo "<pre><code>";
+    var_dump($v);
+    echo "</code></pre>"
+}
+
+    // include('connect.php');
+
+    // try {
+    //     $sql = "SELECT * FROM activities WHERE city_id = :city_id";
+    //     $stmt = $pdo->prepare($sql);
+    //     $stmt->bindValue(":city_id", $_POST['city']);
+    //     $stmt->execute();
+    // } catch(PDOException $e) { 
+    //     echo "<div class='alert alert-error'> ";
+    //     echo "ERROR FETCHING DATABASE: " . $e->getMessage(); 
+    //     echo "</div> ";
+    //     exit(); 
+    // } 
+    // $results = $stmt->fetchAll();
+    //  // var_dump($results);
+    // echo json_encode($results);
+ ?>
